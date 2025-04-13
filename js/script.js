1ab34b91e2f6ca16f8ef977bf3077d9b0164c3eb
AndreasC <andreascalvin21@gmail.com>
2025-04-13 15:55:10 +0700

    fix navbar mobile

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function openNavMobile() {
+  const nav = document.querySelector(".nav-mobile");
+  nav.style.display = "flex";
+}
+
+function closeNavMobile() {
+  const nav = document.querySelector(".nav-mobile");
+  nav.style.display = "none";
+}
